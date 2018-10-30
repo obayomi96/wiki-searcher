ff2758c1ba59393495eb409eb322df257f912f29
obayomi96 <martinsoluwaseun47@gmail.com>
2018-10-30 08:40:34 +0000

    Jquery and AJAX call to the wikipedia API setup ready

---
+++ js/youtube.js
@@ -0,0 +1,39 @@
+function youtubeVid(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
+
+$(function() {
+    $("form").on("submit", function(e) {
+       e.preventDefault();
+       // prepare the request
+       var request = gapi.client.youtube.search.list({
+            part: "snippet",
+            type: "video",
+            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+            maxResults: 10,
+            order: "viewCount",
+       }); 
+       // execute the request
+       request.execute(function(response) {
+          var results = response.result;
+          $("#results").html("");
+          $.each(results.items, function(index, item) {
+            $.get("vid.html", function(data) {
+                $("#results").append(youtubeVid(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
+            });
+          });
+          resetVideoHeight();
+       });
+    });
+    
+    $(window).on("resize", resetVideoHeight);
+});
+
+function resetVideoHeight() {
+    $(".video").css("height", $("#results").width() * 9/16);
+}
+
+function init() {
+    gapi.client.setApiKey("AIzaSyAjb3HIVcI57Cw_mIucJ5jcHmXIBX5fqac");
+    gapi.client.load("youtube", "v3", function() {
+        // yt api is ready
+    });
+}
